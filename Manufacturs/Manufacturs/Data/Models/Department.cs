83f94523bca8420f56ed76014873e5bcc64de320
r-achanova <r.achanova@pmg-pernik.com>
2021-10-21 13:16:22 +0300

    Add classes with data

---
+++ Manufacturs/Manufacturs/Data/Models/Department.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manufacturs.Data.Models
+{
+ public   class Department
+    {
+        public Department()
+        {
+            this.Employees = new HashSet<Employee>();
+        }
+        public int DepartmentId { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Employee> Employees { get; set; }
+
+
+    }
+}
---
+++ Manufacturs/Manufacturs/Data/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manufacturs.Data.Models
+{
+   public class Employee
+    {
+        public int EmployeeId { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+    }
+}
