918751bc39e2368d253a819440023933266665bb
r-achanova <r.achanova@pmg-pernik.com>
2021-10-21 12:22:11 +0300

    Add new project - Manufacturs

---
+++ Manufacturs/Manufacturs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Manufacturs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
